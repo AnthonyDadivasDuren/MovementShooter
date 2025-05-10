6b79448d7dd8579db06b5e9f7d0f6d4902750b7f
Anthony Durén <anthonydadivasduren@gmail.com>
2025-05-10 14:48:07 +0200

    Added a "Rocket Jump" Mechanic for better movement control

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,6 +7,7 @@ public class Gun : MonoBehaviour
     public float range = 100f;
     public float impactForce = 30f;
     public bool isAutomatic = false;
+    public bool isKnockBackable = false;
 
     public float fireRate = 1f;
 
@@ -15,10 +16,15 @@ public class Gun : MonoBehaviour
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0f;
-
+    
 
     private Recoil Recoil_Script;
-
+    
+    [SerializeField] private Rigidbody playerRb;
+    [SerializeField] private float rocketJumpForce = 1000f;
+    [SerializeField] private float rocketJumpRadius = 5f;
+    [SerializeField] private float upwardModifier = 1f;
+    
     void Start()
     {
         Recoil_Script = GameObject.Find("Head/CameraRecoil").GetComponent<Recoil>();
@@ -74,6 +80,19 @@ public class Gun : MonoBehaviour
             GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
 
             Destroy(impactGo, 2f);
+
+            if (isKnockBackable)
+            {
+                // Check if the hit point is close enough to the player
+                Vector3 toPlayer = playerRb.position - hit.point;
+                float distance = toPlayer.magnitude;
+
+                if (distance < rocketJumpRadius)
+                {
+                    playerRb.AddExplosionForce(rocketJumpForce, hit.point, rocketJumpRadius, upwardModifier, ForceMode.Impulse);
+                }
+            }
+            
         }
     }
 }
